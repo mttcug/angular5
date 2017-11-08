65eca0604335e2746abe8e8aee5666ee80b63565
mttcug <mttcug@163.com>
2017-11-08 11:42:58 +0800

    'model'

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -32,18 +32,9 @@ export class AppComponent {
       code: 3,
       name: "123"
     }];
-
-  updateShopName() {
-    console.log("ooo", this.subShopName);
-    this.shopName = this.subShopName == '' ? this.brandName : this.brandName + "(" + this.subShopName + ")";
-    console.log("shopName:", this.shopName);
-  }
-
-
   /* 经营行业*/
   bigIndustry = '';
   smallIndustry = '';
-
   industries = [
     [
       {code: 12, name: "娱乐设施"}, {code: 27895, name: "娱乐"}, {code: 32341, name: "运动"}, {
@@ -69,61 +60,22 @@ export class AppComponent {
       {code: 13, name: "餐饮美食"}, {code: 2123123, name: "娱乐small"}, {code: 3123123, name: "运动small"}
     ]
   ];
-
   bigIndustryList = [
     {code: 1, name: "餐饮"}, {code: 2, name: "娱乐"}, {code: 3, name: "运动"}
   ];
   smallIndustryList = [
     {code: 1, name: "餐饮small"}, {code: 2, name: "娱乐small"}, {code: 3, name: "运动small"}
   ];
-
-
   /*开业日期*/
   startOpenDate = '';
-
-  getStartTime(date) {
-    this.startOpenDate = date;
-    console.log("startOpenDate:", this.startOpenDate);
-  }
-
   /*经营状态*/
   operateStatusList = [
     {code: 1, name: "经营中"}, {code: 2, name: "停业"}, {code: 3, name: "装修"}
   ];
   operateStatus = 1;
-
   /*停业日期*/
   endOpenDate = '';
-
-  getEndTime(date) {
-    this.endOpenDate = date;
-  }
-
-
   /*店铺图片*/
-
-  closeResult: string;
-
-  open(content) {
-    this.modalService.open(content, {size: 'lg'}).result.then((result) => {
-      this.closeResult = `Closed with: ${result}`;
-      console.log("弹出框结果：", result);
-    }, (reason) => {
-      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
-    });
-  }
-
-
-  private getDismissReason(reason: any): string {
-    if (reason === ModalDismissReasons.ESC) {
-      return 'by pressing ESC';
-    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
-      return 'by clicking on a backdrop';
-    } else {
-      return `with: ${reason}`;
-    }
-  }
-
   /*门面*/
   shopImages = [];
   multiShops = true;
@@ -133,22 +85,12 @@ export class AppComponent {
     {id: "123", url: "./app/public/images/2.jpg"}
   ];
   multiEnvironment = true;
-
-  addImages(oldImages, newInamges) {
-    console.log("newInamges:", newInamges);
-    oldImages = newInamges;
-  }
-
   /*前台电话*/
   shopPhoneNumber = '';
-
   /*外卖电话*/
   takeOutPhone = '';
-
-
   /*营业时间*/
   wholeWeek = false;
-
   vaildTime = [
     {code: 1, name: "周一", checked: false},
     {code: 2, name: "周二", checked: false},
@@ -158,43 +100,10 @@ export class AppComponent {
     {code: 6, name: "周六", checked: false},
     {code: 7, name: "周日", checked: false}
   ];
-
-
   operateDate = [];
-
-  selectAllWeek() {
-    this.wholeWeek === false ? this.vaildTime.map((item, index) => {
-      item.checked = true;
-    }) : this.vaildTime.map((item, index) => {
-      item.checked = false;
-    });
-  }
-
-  selectThisDay(item) {
-    if (item.checked === true) {
-      this.operateDate.push(item);
-    } else {
-      this.operateDate.forEach((value, index) => {
-        value.code = item.code ? this.operateDate.splice(index, 1) : "";
-      });
-    }
-  }
-
   allDay = false;
   operateStartTime = '';
   operateEndTime = '';
-
-  selectAllDay() {
-    if (this.allDay === false) {
-      this.operateStartTime = "00:00";
-      this.operateEndTime = "24:00";
-    } else {
-      this.operateStartTime = "";
-      this.operateEndTime = "";
-    }
-  }
-
-
   /*装修档次*/
   fitmentLevelStatus = '';
   fitmentLevelList = [
@@ -207,8 +116,6 @@ export class AppComponent {
     {code: 7, name: "停车位"},
     {code: 8, name: "可刷卡"},
   ];
-
-
   /*提供服务*/
   serviceProvided = [];
   serviceList = [
@@ -221,23 +128,10 @@ export class AppComponent {
     {code: 7, name: "停车位", checked: false},
     {code: 8, name: "可刷卡", checked: false},
   ];
-
-  selectThisService(item) {
-    if (item.checked === false) {
-      this.serviceProvided.push(item);
-    } else {
-      this.serviceProvided.forEach((value, index) => {
-        value.code.toString() === item.code.toString() ? this.serviceProvided.splice(index, 1) : '';
-      });
-    }
-  }
-
   /*店铺租金*/
-  rent = '';
+  shopRent = '';
   rentMeasure = 1;
   rentMeasureList = [{code: 1, name: "元/月"}, {code: 2, name: "万元/月"}, {code: 3, name: "千元/月"}];
-
-
   /*支付方式&&编辑更多支付方式*/
   payWayList = [
     {code: 1, name: "一月一付"},
@@ -248,26 +142,16 @@ export class AppComponent {
     {code: 6, name: "六月一付"},
     {code: 0, name: "自定义"}
   ];
-
   payWay = '';
-  definedPayWay = ''
-
-  selectThisPayWay(item) {
-    console.log("payWay:", item.code, this.payWayList);
-    this.payWay = item.code == 0 ? this.definedPayWay : item.name;
-  }
-
+  definedPayWay = '';
+  /*递增或递减*/
   rateWay = '';
   definedRateWay = '';
   rateChoice = [
     {code: 1, name: "递增"},
-    {code: 2, name: "两月租金"}
+    {code: 2, name: "递减"}
   ];
-
-  selectThisRate(item) {
-    this.rateWay = item.name;
-  }
-
+  /*押金*/
   depositWay = '';
   definedDepositWay = '';
   deposit = [
@@ -279,22 +163,11 @@ export class AppComponent {
     {code: 6, name: "六月租金"},
     {code: 0, name: "自定义"}
   ];
-
-  selectThisDepositWay(item) {
-    this.depositWay = item.code == 0 ? this.definedDepositWay : item.name;
-  }
-
   rentDate = '';           //租期
-  getRentDate(date) {
-    this.rentDate = date;
-  }
-
   rentTime = '';           //租约
   leftContractTime = '';  //剩余合同期
-
   /*客单价*/
   personProfit = '';  //客单价
-
   /*日均营业额*/
   dayProfit = '';       //日均营业额
 
@@ -309,8 +182,6 @@ export class AppComponent {
   memberAmount = ''; //会员数
 
   memberType = '';  //会员类型
-
-
   /*转让状态*/
   transferStatus = ''          //转让状态
   transferStatusList = [
@@ -318,13 +189,7 @@ export class AppComponent {
     {code: 2, name: "转让中"},
     {code: 3, name: "转让成功"},
     {code: 4, name: "租约到期"}
-  ]
-
-  selectTransferStatus(item) {
-    this.transferStatus = item;
-    console.log("1");
-  }
-
+  ];
   /*转让费*/
   transferFee = '';              //转让费
 
@@ -334,11 +199,6 @@ export class AppComponent {
     {code: 1, name: "可空转"},
     {code: 2, name: "不可空转"}
   ];
-
-  selectEmptyTransfer(item) {
-    this.emptyTransfer = item;
-    console.log("2");
-  }
 
   emptyTransferFee = '';          //空转转让费
   transferStaff = '';           //转让内容
@@ -350,13 +210,6 @@ export class AppComponent {
     {id: 2, name: "店铺图片"},
     {id: 3, name: "身份证件"}
   ];
-
-  deleteCertification(item) {
-    this.certifications.forEach((value, index) => {
-      value.id == item.id ? this.certifications.splice(index, 1) : '';
-    });
-  }
-
   /*店铺证件弹出框*/
   selectedCertification = 4;
   certificationType = '';           //证件类型
@@ -379,7 +232,6 @@ export class AppComponent {
   permissionScope = '';         //许可范围
   otherContent = '';            //其他内容
 
-
   /*店铺老板 && 个人信息弹出框*/
   shopBoss = {
     name: '',
@@ -397,28 +249,15 @@ export class AppComponent {
   phoneList = [""];
   name = '';                       //名称
   realName = ''                   //真实名字
-
-  addPhoneNumbers() {
-    this.phoneList.push("");          //增加输入框个数
-  }
-
+  /*性别*/
   sexType = [
     {code: 1, name: "男"},
     {code: 2, name: "女"},
     {code: 3, name: "其他"}
   ];
   sexSelected = '';            //性别
-  selectSexStatus(item) {
-    this.sexSelected = item;
-    console.log("已选性别：", this.sexSelected);
-  }
-
-
+  /*出生日期*/
   birthdayDate = '';
-
-  getbirthTime(date) {
-    this.birthdayDate = date;
-  }
 
   contactAddress = '';            //联系地址
 
@@ -426,7 +265,6 @@ export class AppComponent {
   qq = '';                        //qq
   wx = '';                        //微信
   personInfoDetail = '';          //详细描述
-
 
   /*合伙人*/
   partner = [
@@ -482,27 +320,6 @@ export class AppComponent {
     position: '', entryDate: '', workContent: '', leaveDate: ''
   }];
 
-
-  addEmployee() {
-    var obj = {
-      position: '',
-      entryDate: '',
-      workContent: '',
-      leaveDate: '',
-      phoneList: [],
-      realName: '',
-      sex: '',
-      birthdayDate: '',
-      contactAddress: '',
-      email: '',
-      qq: '',
-      wx: '',
-      personInfoDetail: ''
-    };
-    this.employee.push(obj);
-  }
-
-
   /*所在位置*/
   city = '';           //城市
   district = '';       //区域
@@ -510,16 +327,8 @@ export class AppComponent {
   districtList = [];
   addressDetail = '';
 
-  cityChange(code) {
-
-  }
-
-  districtChange(code) {
-
-  }
-
-  lng = '';                       //经度
-  lat = '';                       //纬度
+  longitude = '';                       //经度
+  latitude = '';                       //纬度
 
   /*位置描述*/
   positionDescription = '';       //位置描述
@@ -544,18 +353,6 @@ export class AppComponent {
   floorAmount = '';                  //总层数
 
 
-  /*适合经营*/
-  /*打开行业列表弹出框的时候确定默认选择列表(三个按钮打开同一个行业列表弹出框)*/
-  industrySelectedList = [];  //用来记录适合经营，推荐经营和不宜经营点击的是哪一个
-  openModel(content, selectedList) {
-    this.industrySelectedList = selectedList;
-    this.modalService.open(content, {size: 'lg'}).result.then((result) => {
-      this.closeResult = `Closed with: ${result}`;
-    }, (reason) => {
-      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
-    });
-  }
-
 
   /*适合经营*/
   fitIndustry = [                   //适合经营
@@ -564,24 +361,6 @@ export class AppComponent {
     {code: 3, name: "养生1"},
     {code: 4, name: "美妆1"}
   ];
-
-  selectThisIndustry(item) {
-    this.industrySelectedList.forEach((v, i) => {
-      if (v.code.toString() == item.code.toString()) {
-        return;
-      } else if (i == (this.industrySelectedList.length - 1) && v.code.toString() != item.code.toString()) {
-        this.industrySelectedList.push(item);
-      }
-    })
-    console.log("default:", this.industrySelectedList, this.fitIndustry);
-  }
-
-  deleteSelectedIndustry(dataList, item) {
-    dataList.forEach((value, index) => {
-      value.code == item.code ? dataList.splice(index, 1) : '';
-    });
-  }
-
   /*推荐经营的行业*/
   recommendableIndustry = [             //推荐经营
     {code: 1, name: "餐馆2"},
@@ -611,22 +390,12 @@ export class AppComponent {
     {code: 8, name: "天然气", checked: false},
     {code: 9, name: "外边去", checked: false}
   ];
-
-  selectThisFacility(item) {
-    this.facilities.forEach((v, i) => {
-      console.log("123", item.code, v.code, v.code.toString() === item.code.toString());
-      v.code.toString() === item.code.toString() ? this.facilities.splice(i, 1) : i == this.facilities.length ? this.facilities.push(item) : '';
-    })
-
-    console.log("fa:", this.facilities);
-  }
-
   /*费用*/
   waterFee = '';                //水费
   facilityFee = '';             ////物业
   gasFee = '';                   //燃气
   elecFee = '';                  //电费
-
+  rent='';                      //租金
   warmFee = '';                    //暖气
 
   /*建筑形状*/
@@ -644,6 +413,266 @@ export class AppComponent {
 
   /*房东*/
   houseOwner = {};
+
+
+
+
+
+  updateShopName() {
+    console.log("ooo", this.subShopName);
+    this.shopName = this.subShopName == '' ? this.brandName : this.brandName + "(" + this.subShopName + ")";
+    console.log("shopName:", this.shopName);
+  }
+
+  getStartTime(date) {
+    this.startOpenDate = date;
+    console.log("startOpenDate:", this.startOpenDate);
+  }
+
+  getEndTime(date) {
+    this.endOpenDate = date;
+  }
+
+  addImages(oldImages, newInamges) {
+    oldImages = newInamges;
+  }
+
+  selectAllWeek() {
+    this.wholeWeek === false ? this.vaildTime.map((item, index) => {
+      item.checked = true;
+    }) : this.vaildTime.map((item, index) => {
+      item.checked = false;
+    });
+  }
+
+  selectThisDay(item) {
+    if (item.checked === true) {
+      this.operateDate.push(item);
+    } else {
+      this.operateDate.forEach((value, index) => {
+        value.code = item.code ? this.operateDate.splice(index, 1) : "";
+      });
+    }
+  }
+
+  selectAllDay() {
+    if (this.allDay === false) {
+      this.operateStartTime = "00:00";
+      this.operateEndTime = "24:00";
+    } else {
+      this.operateStartTime = "";
+      this.operateEndTime = "";
+    }
+  }
+
+  selectThisService(item) {
+    if (item.checked === false) {
+      this.serviceProvided.push(item);
+    } else {
+      this.serviceProvided.forEach((value, index) => {
+        value.code.toString() === item.code.toString() ? this.serviceProvided.splice(index, 1) : '';
+      });
+    }
+  }
+
+  selectThisPayWay(item) {
+    console.log("payWay:", item.code, this.payWayList);
+    this.payWay = item.code == 0 ? this.definedPayWay : item.name;
+  }
+
+  selectThisRate(item) {
+    this.rateWay = item.name;
+  }
+
+  selectThisDepositWay(item) {
+    this.depositWay = item.code == 0 ? this.definedDepositWay : item.name;
+  }
+
+  getRentDate(date) {
+    this.rentDate = date;
+  }
+
+  selectTransferStatus(item) {
+    this.transferStatus = item;
+  }
+
+  selectEmptyTransfer(item) {
+    this.emptyTransfer = item;
+  }
+
+  deleteCertification(item) {
+    this.certifications.forEach((value, index) => {
+      value.id == item.id ? this.certifications.splice(index, 1) : '';
+    });
+  }
+
+  addPhoneNumbers() {
+    this.phoneList.push("");          //增加输入框个数
+  }
+
+  selectSexStatus(item) {
+    this.sexSelected = item;
+  }
+
+  getbirthTime(date) {
+    this.birthdayDate = date;
+  }
+
+  addEmployee() {
+    var obj = {
+      position: '',
+      entryDate: '',
+      workContent: '',
+      leaveDate: '',
+      phoneList: [],
+      realName: '',
+      sex: '',
+      birthdayDate: '',
+      contactAddress: '',
+      email: '',
+      qq: '',
+      wx: '',
+      personInfoDetail: ''
+    };
+    this.employee.push(obj);
+  }
+
+  cityChange(code) {
+
+  }
+
+  districtChange(code) {
+
+  }
+
+  /*适合经营*/
+  /*打开行业列表弹出框的时候确定默认选择列表(三个按钮打开同一个行业列表弹出框)*/
+  industrySelectedList = [];  //用来记录适合经营，推荐经营和不宜经营点击的是哪一个
+  openModel(content, selectedList) {
+    this.industrySelectedList = selectedList;
+    this.modalService.open(content, {size: 'lg'}).result.then((result) => {
+      this.closeResult = `Closed with: ${result}`;
+    }, (reason) => {
+      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+    });
+  }
+
+  selectThisIndustry(item) {
+    this.industrySelectedList.forEach((v, i) => {
+      if (v.code.toString() == item.code.toString()) {
+        return;
+      } else if (i == (this.industrySelectedList.length - 1) && v.code.toString() != item.code.toString()) {
+        this.industrySelectedList.push(item);
+      }
+    })
+    console.log("default:", this.industrySelectedList, this.fitIndustry);
+  }
+
+  deleteSelectedIndustry(dataList, item) {
+    dataList.forEach((value, index) => {
+      value.code == item.code ? dataList.splice(index, 1) : '';
+    });
+  }
+
+  selectThisFacility(item) {
+    this.facilities.forEach((v, i) => {
+      console.log("123", item.code, v.code, v.code.toString() === item.code.toString());
+      v.code.toString() === item.code.toString() ? this.facilities.splice(i, 1) : i == this.facilities.length ? this.facilities.push(item) : '';
+    })
+  }
+
+
+
+
+  ///////////////////////////////////////////////////////////////////////////////////////////////
+  //开始处理弹出框的值
+  ////////////////////////////////////////////////////////////////////////////////////////////////
+  /*图片弹出框*/
+  tempshopImages=[{id:1,url:"http://www.baidu.com"},{id:2,url:"http://www.baidu.com"}];       //此处复制方式和shopImages一样；
+  tempenvironment=[{id:3,url:"http://www.baidu.com"}];
+  jsonShopImages={tempshopImages:this.tempshopImages,tempenvironment:this.tempenvironment};
+
+
+
+  /*店铺租金弹出框*/
+  tempshopRent = '';
+  temprentMeasure = 1;
+  temprateWay = '';
+  tempdefinedRateWay = '';
+  tempdepositWay = '';
+  tempdefinedDepositWay = '';
+  temprentDate = '';           //租期
+  temprentTime = '';           //租约
+  templeftContractTime = '';  //剩余合同期
+  tempRentContent={
+    tempshopRent:this.tempshopRent,
+    temprentMeasure:this.temprentMeasure,
+    temprateWay:this.temprateWay,
+    tempdefinedRateWay:this.tempdefinedRateWay,
+    tempdepositWay:this.tempdepositWay,
+    tempdefinedDepositWay:this.tempdefinedDepositWay,
+    temprentDate:this.temprentDate,
+    temprentTime:this.temprentTime,
+    templeftContractTime:this.templeftContractTime
+  }
+
+
+  /*店铺营业状况弹出框*/
+  temppersonProfit='';
+  tempdayProfit='';
+  tempconsumePersonType='';
+  tempconsumeTime='';
+  tempfoodAmount='';
+  temptakeOutAmount='';
+  tempmemberAmount='';
+  tempmemberType='';
+
+  tempOperateStatus={
+    temppersonProfit:this.temppersonProfit,
+    tempdayProfit:this.tempdayProfit,
+    tempconsumePersonType:this.tempconsumePersonType,
+    tempconsumeTime:this.tempconsumeTime,
+    tempfoodAmount:this.tempfoodAmount,
+    temptakeOutAmount:this.temptakeOutAmount,
+    tempmemberAmount:this.tempmemberAmount,
+    tempmemberType:this.tempmemberType
+  }
+
+  /*转让信息弹出框*/
+
+
+
+
+
+
+
+  ////////////////////////////////////////////////////////////////////////////////////////////////
+  //公共函数
+  closeResult: string;
+  open(content) {
+    this.modalService.open(content, {size: 'lg'}).result.then((result) => {
+      this.sureBtnFunction(content,result);
+    }, (reason) => {
+      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+    });
+  }
+
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a backdrop';
+    } else {
+      return `with: ${reason}`;
+    }
+  }
+
+  sureBtnFunction(content,result){
+
+  }
+
+
+
 
 
 }
